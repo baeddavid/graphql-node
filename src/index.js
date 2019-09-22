1155bd434b0358f4a92349c4e9976512883b2550
David Bee <baeddavid@gmail.com>
2019-09-22 11:51:25 -0500

    add crud for api

--- src/index.js
+++ src/index.js
@@ -1,37 +1,52 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
-type Link {
-    id: ID!
-    description: String!
-    url: String!    
-}
-`
-
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Full Stack Tutorial for GraphQL',
 }]
-
+let idCount = links.length;
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         feed: () => links,
+        link: (root, {id}) => links.find(link => link.id == id)
     },
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount}++`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link);
+            return link;
+        },
+
+        updateLink: (parent, args) => {
+            let updatedLink;
+
+            links = links.map(link => {
+                if(link.id == args.id) {
+                    updatedLink = {...link, ...args};
+                    return updatedLink;
+                }
+                return link;
+            });
+            return updatedLink;
+        }, 
+
+        deleteLink: (root, args) => {
+            const removeIndex = links.find(item => item.id == args.id);
+            const removedLink = links[removeIndex];
+            links.splice(removeIndex, 1);
+            return removedLink;
+        }
     }
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './schema.graphql',
     resolvers
 });
 
